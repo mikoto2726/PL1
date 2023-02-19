a619d0da1f9da8275145a3f6ad20bdf23d2ac89c
s1f10220137 <s1f102201379@iniad.org>
2023-02-19 15:29:17 +0900

    1

--- 04/fib.c
+++ 04/fib.c
@@ -17,7 +17,7 @@ int main(){
 #include <stdio.h>
 unsigneed int fib(unsigned int n){
     unsigned int f[2] = {0, 1};
-    for (unsigned int i = 2; i <= n; i++){
+    for (unsigned int i = 2; i <= n; i++){  // 配列fは、2つの要素しか持っていないため、iが偶数の場合と奇数の場合で、使用する要素が切り替わります。具体的には、 f[i % 2] は、iが偶数の場合には f[0] に、iが奇数の場合には f[1] に対応します。これにより、i番目のフィボナッチ数を計算するために必要な2つの要素を常に更新することができます。
         f[i % 2] = f[(i-1) % 2] + f[(i-2) % 2];
     }
     return f[n % 2];
