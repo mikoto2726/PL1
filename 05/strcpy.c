876a0280ed853115731914252fdc968d379b1117
s1f10220137 <s1f102201379@iniad.org>
2023-04-04 04:38:10 +0900

    1

---
+++ 05/strcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+char *my_strcpy(char *to, const char* from){
+    size_t i;
+    for (i = 0; from[i] != '\0'; i++){
+        to[i] = from[i];
+    }
+    to[i] = '\0';
+    return to;
+}
+
+int main(){
+    char s[20], t[20];
+    strcpy(s, "INIAD");
+    my_strcpy(t, "Toyo");
+    printf("s = %s, t = %s\n", s, t);
+    return 0;
+}
