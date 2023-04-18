2b614328b915851fb34d398f0f297e26e579005a
s1f10220137 <s1f102201379@iniad.org>
2023-04-18 10:45:09 +0900

    1

---
+++ 02/pretest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n, i, result;
+    printf("n= ");
+    scanf("%d", &n);
+    result = 0;
+    for (i = 1; i <= n; i++){
+        result += i * i * i * i;
+    }
+    printf("sigma(n) = %d\n", result);
+    return 0;
+}
